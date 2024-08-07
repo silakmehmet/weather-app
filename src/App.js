bfd58893f5b78e30097036866b8757ef636d6b2b
silakmehmet <silakmehmet@gmail.com>
2024-08-07 21:35:23 +0300

    Input and Button added

--- src/App.js
+++ src/App.js
@@ -1,17 +1,27 @@
 import "./App.css";
+import logo from "./images/logo.png";
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
-import logo from "./images/logo.png";
+import Button from "react-bootstrap/Button";
 
 function App() {
   return (
     <div className="app-container">
       <Container>
         <Row>
-          <header className="h1">Weather App</header>
+          <header className="h1 text-white-50 fw-bold">Weather App</header>
         </Row>
         <Row>
-          <section>Display and search bar</section>
+          <section>
+            <input
+              type="text"
+              placeholder="Search for a city"
+              className="my-4 me-2 border border-0 rounded p-2"
+            />
+            <Button variant="success" className="my-4 p-2">
+              SEARCH
+            </Button>
+          </section>
         </Row>
         <Row>
           <footer className="position-relative">
